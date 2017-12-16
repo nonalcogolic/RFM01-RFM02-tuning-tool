72188715eeaccef6a5511e9ca09fc07078f413aa
Aleksey <OSmiian@luxoft.com>
2017-12-16 12:10:35 +0200

    tr modified read status

--- crfmtransmitterhandler.cpp
+++ crfmtransmitterhandler.cpp
@@ -45,6 +45,7 @@ void CRFMTransmitterHandler::sendData(const std::vector<bool> command, const std
       mPinout.setPinState(bit, ePin::tr_SDI);
    }
 
+   mPinout.setPinState(true, ePin::tr_SCK);
    mPinout.setPinState(true, ePin::tr_nSEL);
 }
 
@@ -56,19 +57,25 @@ std::vector<bool> CRFMTransmitterHandler::readStatus()
    mPinout.setPinState(false, ePin::tr_nSEL);
    mPinout.setPinState(false, ePin::tr_SDI);
 
-   auto command = std::vector<bool>{1,1,0,0,  1,1,0,0,  0,0,0,0,  0,0,0,0};
+   auto command = std::vector<bool>{1,1,0,0,  1,1,0,0};
 
-   for (int i = 0; i<16; ++i)
+   for (int i = 0; i<8; ++i)
    {
       mPinout.setPinState(command[i], ePin::tr_SDI);
       mPinout.setPinState(true, ePin::tr_SCK);
-
-      if (i >= 8)
-         output.push_back(mPinout.getPinState(ePin::tr_NIRQ));
-
       mPinout.setPinState(false, ePin::tr_SCK);
    }
 
+   for (int i = 0; i<7; ++i)
+   {
+      output.push_back(mPinout.getPinState(ePin::tr_NIRQ));
+      mPinout.setPinState(true, ePin::tr_SCK);
+      mPinout.setPinState(false, ePin::tr_SCK);
+   }
+   output.push_back(mPinout.getPinState(ePin::tr_NIRQ));
+
+
+   mPinout.setPinState(true, ePin::tr_SDI);
    mPinout.setPinState(true, ePin::tr_nSEL);
 
    return output;
